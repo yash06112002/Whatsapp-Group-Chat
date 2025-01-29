998d15e1079293d7d17ed2e140a83be152362a13
yash06112002 <yashkansara0611@gmail.com>
2025-01-29 20:05:27 +0530

    back button, open last message

--- src/Chat.js
+++ src/Chat.js
@@ -1,18 +1,17 @@
-import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined, MicOutlined } from '@mui/icons-material';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined, MicOutlined, KeyboardBackspace } from '@mui/icons-material';
 import { Avatar, IconButton } from '@mui/material';
-import React, { useState, useEffect, } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useStateValue } from './StateProvider';
 import './Chat.css';
-// import axios from './axios';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import db from './firebase';
-// import firebase from 'firebase/compat';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 function Chat() {
-
+    const navigate = useNavigate();
+    const latestMessageRef = useRef(null);
     const { roomId } = useParams();
     const [input, setInput] = useState("");
     const [seed, setSeed] = useState("");
@@ -28,17 +27,23 @@ function Chat() {
             db.collection('rooms').doc(roomId).collection('messages').orderBy('timestamp', 'asc').onSnapshot(snapshot => (
                 setMessages(snapshot.docs.map(doc => doc.data()))
             ))
+            setSeed(Math.floor(Math.random() * 5000))
         }
-        setSeed(Math.floor(Math.random() * 5000))
     }, [roomId])
 
     useEffect(() => {
         document.querySelector('.sidebar').classList.add('inactive');
-
+        
         return () => {
             document.querySelector('.sidebar').classList.remove('inactive');
         }
     }, [])
+
+    useEffect(() => {
+        if (latestMessageRef.current) {
+            latestMessageRef.current.scrollIntoView({ behavior: "auto" }); // No animation, just position directly
+        }
+    }, [messages])
 
     const sendMessage = async (e) => {
         e.preventDefault();
@@ -54,7 +59,11 @@ function Chat() {
     return (
         <div className='chat'>
             <div className='chat_header'>
-                <Avatar src={`https://avatars.dicebear.com/api/male/${seed}.svg`} />
+                <div className='chat_headerBack'>
+                <IconButton>
+                    <KeyboardBackspace onClick={() => navigate('/')} />
+                </IconButton>
+                </div>
                 <h3 className='chat_headerInfo'>
                     {roomName}
                 </h3>
@@ -71,8 +80,11 @@ function Chat() {
                 </div>
             </div>
             <div className='chat_body'>
-                {messages.map((message) => (
-                    <p className={`chat_message ${message.name === user.displayName && 'chat_reciever'}`}>
+                {messages.map((message, index) => (
+                    <p
+                        className={`chat_message ${message.name === user.displayName && 'chat_reciever'}`}
+                        ref={index === messages.length - 1 ? latestMessageRef : null}
+                    >
                         <span className='chat_name'>{message.name}</span>
                         {message.message}
                         <span className='chat_timestamp'>
