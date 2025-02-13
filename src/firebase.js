b91404c0e3ed30ba03f388b7f634c14be3d68c14
yash06112002 <yashkansara0611@gmail.com>
2025-02-13 21:39:41 +0530

    firebase project change

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCbZsQFS7G3Uo9oE_f1aIVw-P3yMCcKZXs",
-    authDomain: "whatsapp-yk.firebaseapp.com",
-    projectId: "whatsapp-yk",
-    storageBucket: "whatsapp-yk.appspot.com",
-    messagingSenderId: "14281047747",
-    appId: "1:14281047747:web:e633f4652cc4972742ec76"
-};
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
